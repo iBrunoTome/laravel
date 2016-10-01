64b8fcc7ec73e30629e9c4d6eb7621c9a9ef3797
Bruno Tomé <ibrunotome@gmail.com>
2016-10-01 11:46:30 -0300

    Trabalhando com models - Exibindo clients em json

--- app/User.php
+++ app/User.php
@@ -1,29 +1,29 @@
 <?php
 
-namespace App;
+	namespace CodeProject;
 
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+	use Illuminate\Foundation\Auth\User as Authenticatable;
+	use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-}
+	class User extends Authenticatable {
+		use Notifiable;
+		/**
+		 * The attributes that are mass assignable.
+		 *
+		 * @var array
+		 */
+		protected $fillable = [
+			'name',
+			'email',
+			'password',
+		];
+		/**
+		 * The attributes that should be hidden for arrays.
+		 *
+		 * @var array
+		 */
+		protected $hidden = [
+			'password',
+			'remember_token',
+		];
+	}
